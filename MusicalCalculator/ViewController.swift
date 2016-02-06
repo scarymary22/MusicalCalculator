3e26d685377dbdbfc8032f2c77b245fed75518bc
Mary Dye <marywdye@me.com>
2016-02-06 17:12:27 -0500

    Initial Commit

---
+++ MusicalCalculator/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  MusicalCalculator
+//
+//  Created by Mary Dye on 2/6/16.
+//  Copyright © 2016 Mary Dye. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
