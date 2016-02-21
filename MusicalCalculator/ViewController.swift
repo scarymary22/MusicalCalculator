f4bac0206296675806a1b083f1ec8ec54831a966
Mary Dye <marywdye@me.com>
2016-02-21 15:37:22 -0500

    committing first changes to project

--- MusicalCalculator/ViewController.swift
+++ MusicalCalculator/ViewController.swift
@@ -10,6 +10,8 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet var displayField: UILabel?
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
@@ -19,7 +21,24 @@ class ViewController: UIViewController {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
+    
+    @IBAction func onePressed(theButton: UIButton) {
+        // This is what will happen when the One button is pressed
+        print("onePressed")
+        displayField?.text = theButton.titleLabel?.text
+    }
 
+    @IBAction func twoPressed(theButton: UIButton) {
+        // This is what will happen when the two button is pressed
+        print("twoPressed")
+        displayField?.text = theButton.titleLabel?.text
+    }
+
+    @IBAction func plusPressed(theButton: UIButton) {
+        // This is what will happen when the plus button is pressed
+        print("plusPressed")
+        displayField?.text = theButton.titleLabel?.text
+    }
 
 }
 
